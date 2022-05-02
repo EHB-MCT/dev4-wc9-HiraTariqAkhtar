90cf2e3cd3f5c70a208842b15da8114403f2e053
HiraTariqAkhtar <hira.akhtar@student.ehb.be>
2022-05-02 11:31:29 +0200

    ex1: number of increasements

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -1,9 +1,42 @@
 fun main(args: Array<String>) {
     val reader = FileReader();
 
-    println(reader.getPuzzle1())
+    //println(reader.getPuzzle1())
+    val puzzle1 = reader.getPuzzle1()
+    oef1a(puzzle1)
+    oef1b(puzzle1)
+
     //println(reader.getPuzzle2())
     //println(reader.getPuzzle3())
     //println(reader.getPuzzle4Numbers())
     //println(reader.getPuzzle4Cards())
+}
+
+fun oef1a(p : List<Int>){
+    var numberOfIncreasement = 0
+    var previous = p[0]
+
+    p.forEach{
+        if (it > previous){
+            numberOfIncreasement ++
+        }
+        previous = it
+    }
+
+    println(numberOfIncreasement)
+}
+
+fun oef1b(p: List<Int>){
+    var increases = 0
+    var prevNumber = p[0] + p[1] + p[2]
+    p.forEachIndexed { index, i ->
+        if (index + 2 in p.indices){
+            var sum = p[index] + p[index +1] + p[index +2]
+            if (sum > prevNumber){
+                increases ++
+            }
+            prevNumber = sum
+        }
+    }
+    println(increases)
 }
